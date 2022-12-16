5513a3a32bc4673207ea383feba07cd9d7520520
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-16 17:02:39 -0300

    F117 - HTTP requests

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,16 +1,35 @@
- 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
 
-MoviesProvider() {
-  print('MoviesProvider init');
+  String _apiKey = '840895056f195254dea7bdbadd6075a6';
+  String _baseUrl = 'api.themoviedb.org'; //* no hace falta el http ya que Uri lo coloca
+  String _language = 'es-ES';
 
-  getOntDisplayMovies();
-}
+  MoviesProvider() {
+ 
+    print('MoviesProvider init');
 
-getOntDisplayMovies() async {
-  print('getOnDisplayMovies');
-}
+    getOntDisplayMovies();
+  }
+
+  getOntDisplayMovies() async {
+    //print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
+      'api_key' : _apiKey,
+      'language' : _language,
+      'page' : '1'
+      });
+
+    final response = await http.get(url);
+    // if(response.statusCode != 200) return print('error');
+    //final decodedData = json.decode(response.body) as Map<String, dynamic>;
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print(decodedData['results']);
+
+  }
 
 }
