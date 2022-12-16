77096e3fa98c369f069c6ebca63f9045e71cb3a0
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-16 13:21:53 -0300

    F112 - MovieDetails - Slivers - SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-
   const DetailsScreen({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-      body: Container(
-      child: const Center(
-        child: Text('Details Screen')
+    //TODO: Cambiar luego por una instancia de movie
+
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return const Scaffold(
+        body: CustomScrollView(
+      slivers: [_CustomAppBar()],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle( fontSize: 16),
+            )
         ),
+
+        background: FadeInImage(
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage('https://via.placeholder.com/500x300'),
+            fit: BoxFit.cover),
       ),
     );
   }
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MovieSlider extends StatelessWidget {
   const MovieSlider({super.key});
 
@@ -10,29 +9,24 @@ class MovieSlider extends StatelessWidget {
       width: double.infinity,
       height: 260,
       //color: Colors.red,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        
-        const Padding (
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Text('Populares', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Text('Populares',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         ),
-
-        SizedBox(height: 5,),
-
+        SizedBox(
+          height: 5,
+        ),
         Expanded(
           child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 20, 
-            // itemBuilder: ( _ , int index){
-            //   return _MoviePoster();
-            // },
-            itemBuilder: ( _ , int index) => _MoviePoster()
-          ),
+              scrollDirection: Axis.horizontal,
+              itemCount: 20,
+              // itemBuilder: ( _ , int index){
+              //   return _MoviePoster();
+              // },
+              itemBuilder: (_, int index) => _MoviePoster()),
         ),
-
       ]),
     );
   }
@@ -45,35 +39,33 @@ class _MoviePoster extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 130,
-      height: 190,
-      //color: Colors.green,
-      margin: EdgeInsets.symmetric( horizontal: 10),
-      child: Column(
-        children: [
-
+        width: 130,
+        height: 190,
+        //color: Colors.green,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
             GestureDetector(
-              onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
-              child: ClipRRect(//para borderradius
-              borderRadius: BorderRadius.circular(20),
+              onTap: () => Navigator.pushNamed(context, 'details',
+                  arguments: 'movie-instance'),
+              child: ClipRRect(
+                //para borderradius
+                borderRadius: BorderRadius.circular(20),
                 child: FadeInImage(
-                  placeholder: AssetImage('assets/no-image.jpg'),
-                  image: NetworkImage('https://via.placeholder.com/300x400'),
-                  width: 130,
-                  height: 190,
-                  fit: BoxFit.cover
-                  ),
+                    placeholder: AssetImage('assets/no-image.jpg'),
+                    image: NetworkImage('https://via.placeholder.com/300x400'),
+                    width: 130,
+                    height: 190,
+                    fit: BoxFit.cover),
               ),
             ),
-
-
-              Text(
-                'Star Wars: El retorno de el nuevo jedi silvestre de monte cristo',
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                )
-        ],)
-    );
+            Text(
+              'Star Wars: El retorno de el nuevo jedi silvestre de monte cristo',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+            )
+          ],
+        ));
   }
 }
