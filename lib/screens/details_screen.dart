3fefbab9ce7c1bf495d0b1afae76b70fe16ad5d8
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-16 13:52:20 -0300

    F113 - Poster and movie title

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -7,13 +7,20 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     //TODO: Cambiar luego por una instancia de movie
 
-    final String movie =
-        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
 
     return const Scaffold(
         body: CustomScrollView(
-      slivers: [_CustomAppBar()],
-    ));
+          slivers: [
+            _CustomAppBar(),
+            SliverList(
+              delegate: SliverChildListDelegate.fixed([
+                _PosterAndTitle(),
+              ]),
+              )
+          ],
+        )
+      );
   }
 }
 
@@ -45,6 +52,51 @@ class _CustomAppBar extends StatelessWidget {
             image: NetworkImage('https://via.placeholder.com/500x300'),
             fit: BoxFit.cover),
       ),
+    );
+  }
+}
+
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric( horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150
+
+            )
+          ),
+
+          const SizedBox( width: 20, ),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie.title', style: textTheme.headline5, maxLines: 2,),
+              Text('movie.originalTitle', style: textTheme.subtitle1, maxLines: 1,),
+
+              Row(children: [
+                Icon(Icons.star_outline, size: 15, color: Colors.grey,),
+                 SizedBox( width: 5, ),
+                Text('movie.voteAverage', style: textTheme.caption,)
+              ],)
+            ],
+          )
+        ],
+      )
     );
   }
 }
