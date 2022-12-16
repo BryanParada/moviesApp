abf6ffd229b4a87664c21a033d38eadfbb055cdb
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-16 10:37:03 -0300

    F109 - Swiper - carousel cards

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,15 +1,39 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 
 class CardSwiper extends StatelessWidget {
   const CardSwiper({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+ 
     return Container(
       width: double.infinity,
-      height: 500,
-      color: Colors.red,
+      height: size.height * 0.5,
+      //color: Colors.red,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.4,
+        itemBuilder: ( _ , int index){
+
+          return GestureDetector(//permite añadir onTap
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            child: ClipRRect(//pora añadir border radius
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                fit: BoxFit.cover //<- para adaptar imagen al tamaño que tiene el contenedor padre
+                ),
+            ),
+          );
+        },
+        ),
     );
   }
 }
